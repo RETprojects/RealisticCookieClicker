70759eb70f7b672f4c560066424259e17d876c11
RETprojects <77463120+RETprojects@users.noreply.github.com>
2023-05-04 03:19:14 -0500

    Update game.py

--- game.py
+++ game.py
@@ -14,15 +14,18 @@ counter = 0
 def increment():
     global counter
     counter += 1
-    label['text'] = f'{counter} clicks'
-    # if the counter goes to a number divisible by 10, show a message
+    label['text'] = f'{counter} clicks' # update the label
+    # if the counter goes to a number divisible by 10, show a message:
     if counter % 10 == 0:
         messagebox.showinfo("showinfo", "GREAT! Can you click another 10 times?")
 
 root = tk.Tk()
 root.title("Realistic Cookie Clicker")
+# the label where the app will display the number of clicks:
 label = tk.Label(root, text='0 clicks', font=('Comic Sans MS', 15))
 label.grid(row=4, column=2)
+# the cookie photo to be displayed on the button:
 photo = ImageTk.PhotoImage(Image.open("640px-Pep-Farm-Cookie-Alt.png").resize((380, 270)))
-tk.Button(root, command=increment, text="Click Me", width=380, height=270, image=photo).grid(row=2, column=2)
+# every time this button is pressed, the counter is incremented:
+tk.Button(root, command=increment, width=380, height=270, image=photo).grid(row=2, column=2)
 root.mainloop()
